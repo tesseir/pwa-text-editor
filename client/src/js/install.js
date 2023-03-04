3518e875501737594df88faeb0a32e5279163da0
tesseir <tesseir.edu@gmail.com>
2023-03-03 18:07:51 -0600

    pdwa work complete thanks to gary's help

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,7 @@
-const butInstall = document.getElementById('buttonInstall');
 
-/*
-  Here we need to provide the logic for when and how to install the PWA. 
-*/
+//! please note that the instructor has provided some of the code for this assignment to the class.
+
+const butInstall = document.getElementById('buttonInstall');
 
 //  Keep this code as-is
 window.addEventListener('beforeinstallprompt', (event) => {
@@ -20,11 +19,9 @@ butInstall.addEventListener('click', async () => {
 
   promptEvent.prompt();
 
-  // TODO: Notice line 9 above. We want the same line here, but the value should be set to null now.
-  // Insert line here
+  window.deferredPrompt = null;
 
-  // TODO: Notice line 10 above. We want the same line here, but the hidden value should be set to true.
-  // Insert line here
+  butInstall.classList.toggle("hidden", true);
 });
 
 window.addEventListener('appinstalled', (event) => {
